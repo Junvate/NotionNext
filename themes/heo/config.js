5fd1a7e79866d358b47683760b6cf2b243c01530
JunchaoYou <122899170+Junvate@users.noreply.github.com>
2025-08-15 20:57:51 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '前往全网最强ai镜像站', url: 'https://www.chatgptnet.com/' },
+    { title: '体验全模型ai', url: 'https://www.chatgptnet.com/' }
   ],
 
   // 英雄区左右侧组件颠倒位置
